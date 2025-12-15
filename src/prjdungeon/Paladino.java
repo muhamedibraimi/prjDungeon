5c8e5aa0da80e7adfc3f0f7db02d598daf9e4fd0
muhamed.ibraimi <muhamed.ibraimi@AULA75-PC25.itib.local>
2025-12-15 10:57:34 +0100

    Progetto Dungeon

--- src/prjdungeon/Paladino.java
+++ src/prjdungeon/Paladino.java
@@ -9,11 +9,42 @@ package prjdungeon;
  * @author muhamed.ibraimi
  */
 public class Paladino extends Personaggio {
-    Personaggio p;
-    int energiaDivina;
+    int enDivina;
+    int enDivinaMax;
     
     public Paladino(String nome, int[] pos, int atk, int def, int hp, int maxHp, double range, int energiaDivina){
         super(nome, pos, atk, def, hp, maxHp, range);
-        this.energiaDivina=energiaDivina;
+        this.enDivina=enDivina;
+        enDivinaMax=energiaDivina;
     }
+    
+    public int attaccoDivino(Personaggio nemico){
+        int danno=0;
+        if(enDivina>0){
+            danno=(getAtk()*3-nemico.getDef()*2)+enDivina;
+            enDivina-=(enDivinaMax/2);
+            if(danno<=0){
+                danno=1;
+            }
+        }
+        return danno;
+    }
+    public int curaDivina(){
+        int hp = getHp();
+        int cura = (enDivina/2);
+        if(enDivina>0){
+            hp+=(enDivina/2);
+            enDivina-=(enDivinaMax/2);
+        }
+        return cura;
+    }
+
+    @Override
+    public String toString() {
+        String txt = super.toString() +"Paladino{" + "enDivina=" + enDivina + ", enDivinaMax=" + enDivinaMax + '}';
+        return txt;
+    }
+    
+    
+    
 }
